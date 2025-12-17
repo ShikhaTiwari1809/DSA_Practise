04cb34f41c77c592c12ac3b04b4963b3ff0bd006
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2025-12-16 20:22:11 -0500

    Time: 3 ms (90.69%) | Memory: 21.2 MB (60.92%) - LeetSync

---
+++ Arrays_and_string/347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,11 @@
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        element_cnt = Counter(nums)
+        #print(element_cnt)
+        sorted_items_desc = sorted(element_cnt.items(), key=lambda item: item[1], reverse=True)
+        sorted_dict_desc = dict(sorted_items_desc)
+        return(list(sorted_dict_desc.keys())[:k])
+        
+
+        
