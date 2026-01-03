1f40634c6aab8dc42d417602d54be992c5c84bf7
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2026-01-02 19:31:00 -0500

    Time: 0 ms (100.00%) | Memory: 18.5 MB (87.12%) - LeetSync

--- searching_algorithm/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ searching_algorithm/34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,23 +1,29 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        l =0
-        n=len(nums)
-        r=n-1
-
-        while l<=r:
-            mid = (l+r)//2
-            print(mid)
-            if nums[mid]==target:
-                start = mid
-                end = mid
-                while end+1<n and nums[end+1]==target: 
-                    end = end+1
-                while start-1 >=0 and nums[start-1]==target:
-                    start = start-1
-                return [start,end]
-            elif target<nums[mid]:
-                r=mid-1
-            else:
-                l=mid+1
         
-        return[-1,-1]
+        def binarySearch(nums,target,leftBias):
+            l =0
+            r=len(nums)-1
+            i=-1
+
+            while l<=r:
+                mid = (l+r)//2
+    
+                if target<nums[mid]:
+                    r=mid-1
+                elif target>nums[mid]:
+                    l=mid+1
+                else:
+                    i=mid
+                    if leftBias:
+                        r=mid-1
+                    else:
+                        l=mid+1
+            return i
+        
+        left = binarySearch(nums,target,True)
+        right = binarySearch(nums,target,False)
+        return [left,right]
+
+        
+        
