b19b2625d81a6c03708ea9b240e84378237f6e61
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2026-01-06 10:15:13 -0500

    Time: 14 ms (87.13%) | Memory: 19.4 MB (12.58%) - LeetSync

--- Sliding_window/567-permutation-in-string/permutation-in-string.py
+++ Sliding_window/567-permutation-in-string/permutation-in-string.py
@@ -1,20 +1,28 @@
 from collections import Counter
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        window = len(s1)
-        l =0
-        r = l+window
-    
-        s2list = list(s2)
-        s1list = list(s1)
-        while r<=len(s2list):
-            if set(s1list) == set(s2list[l:r]):
-                if Counter(s1list)==Counter(s2list[l:r]):
-                    return True
-            
-            r +=1
-            l +=1
-        return False
+        n = len(s1)
+        m= len(s2)
+        if n > m:
+            return False
 
+        s1cnt =[0]*26
+        s2cnt =[0]*26
+        base = ord('a')
 
+        #count of s1 characters
+        for ch in s1:
+            s1cnt[ord(ch) - base] += 1
         
+        for i in range(n):
+            s2cnt[ord(s2[i]) - base]+=1
+        
+        if s1cnt == s2cnt:
+            return True
+
+        for r in range(n,m):
+            s2cnt[ord(s2[r]) - base]+=1
+            s2cnt[ord(s2[r-n]) - base]-=1
+            if s1cnt == s2cnt:
+                return True
+        return False
