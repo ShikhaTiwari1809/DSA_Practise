abf9902f678c010cfa11fa9780d1fd554515de68
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2026-01-13 12:04:09 -0500

    Time: 310 ms (44.73%) | Memory: 21.2 MB (27.32%) - LeetSync

---
+++ searching_algorithm/808-number-of-matching-subsequences/number-of-matching-subsequences.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        
+        from collections import defaultdict, deque
+from typing import List
+
+class Solution:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        waiting = defaultdict(deque)
+
+        # Put each word into the bucket of its first needed character
+        for w in words:
+            waiting[w[0]].append((w, 0))  # (word, index of next char to match)
+
+        ans = 0
+
+        for ch in s:
+            bucket = waiting[ch]
+            waiting[ch] = deque()  # clear; we'll refill with advanced states
+
+            while bucket:
+                w, i = bucket.popleft()
+                i += 1  # matched this char
+                if i == len(w):
+                    ans += 1
+                else:
+                    waiting[w[i]].append((w, i))
+
+        return ans
