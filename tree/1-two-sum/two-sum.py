a553ca4be15ab6c9a15134b0a9ca2a2ef7eb4558
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2026-02-02 11:01:24 -0500

    Time: 4 ms (40.79%) | Memory: 20.6 MB (10.18%) - LeetSync

--- tree/1-two-sum/two-sum.py
+++ tree/1-two-sum/two-sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
+        
         mapp = defaultdict()
         for i, num in enumerate(nums):
-            print(i, num)
+            
             alternate = target - num
             if alternate in mapp.keys():
                 return [mapp[alternate],i]
