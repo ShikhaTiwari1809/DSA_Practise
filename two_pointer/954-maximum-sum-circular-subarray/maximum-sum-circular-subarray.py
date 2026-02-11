17905a8d6b6ee672d9004dc1e5b43d305bf00e50
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2026-02-11 14:50:25 -0500

    Time: 43 ms (76.43%) | Memory: 24.1 MB (58.06%) - LeetSync

---
+++ two_pointer/954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        total = 0
+        
+        # For max subarray (Kadane)
+        curr_max = best_max = nums[0]
+        
+        # For min subarray (Kadane variant)
+        curr_min = best_min = nums[0]
+        
+        total = nums[0]
+        
+        for x in nums[1:]:
+            total += x
+            
+            curr_max = max(x, curr_max + x)
+            best_max = max(best_max, curr_max)
+            
+            curr_min = min(x, curr_min + x)
+            best_min = min(best_min, curr_min)
+        
+        # If all numbers are negative, best_max is the answer
+        if best_max < 0:
+            return best_max
+        
+        return max(best_max, total - best_min)
+
+        
