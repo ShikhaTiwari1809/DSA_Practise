14e22b48f9a909c592fd9f4d497a4c2bc2a08ad3
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2026-02-12 15:40:00 -0500

    Time: 2 ms (54.70%) | Memory: 22.8 MB (24.34%) - LeetSync

--- tree/513-find-bottom-left-tree-value/find-bottom-left-tree-value.py
+++ tree/513-find-bottom-left-tree-value/find-bottom-left-tree-value.py
@@ -6,7 +6,8 @@
 #         self.right = right
 class Solution:
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
-        
+        # BFS Technique on this I will append right value first so at each level values will be from right to left and last value will
+        # be the leftmost value , instead of using that we can also do normal left to right and just use ans = q[0].val basically at     each level pick first value outside for loop
         q = deque([root])
         left_node = None
 
@@ -25,3 +26,21 @@ class Solution:
                 
         
         return left_node
+
+        # DFS Technique in this we will store max_depth and at each level we will only xhnage the answer value if we have not seen the that level or only higher value since we are doing left first so at each depth we will get left most value of that depth level.
+        self.max_depth = -1
+        self.answer = 0
+        
+        def dfs(node, depth):
+            if not node:
+                return
+            
+            if depth > self.max_depth:
+                self.max_depth = depth
+                self.answer = node.val
+            
+            dfs(node.left, depth + 1)
+            dfs(node.right, depth + 1)
+        
+        dfs(root, 0)
+        return self.answer
