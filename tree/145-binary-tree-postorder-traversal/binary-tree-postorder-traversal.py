0239b777860c0722e7ba362354ea33b7ec736a61
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2026-02-12 20:02:30 -0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (45.43%) - LeetSync

--- tree/145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
+++ tree/145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -7,35 +7,67 @@
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         #Iterative solution
+        def iterativeReverse():
+            if not root:
+                return []
 
-        if not root:
-            return []
+            stack = [root]
+            result = []
 
-        stack = [root]
-        result = []
+            while stack:
+                node = stack.pop()
+                result.append(node.val)          # Root first
+                
+                if node.left:
+                    stack.append(node.left)   # push left
+                if node.right:
+                    stack.append(node.right)  # push right (so right is processed first)
 
-        while stack:
-            node = stack.pop()
-            result.append(node.val)          # Root first
+            return result[::-1]
+        
+        def iterativeLastVisited():
+            #Iterative solution with last visisted without reversing
+
+            result = []
+            stack = []
+            curr = root
+            lastVisited = None
+
+            while curr or stack:
+                # go left as far as possible
+                while curr:
+                    stack.append(curr)
+                    curr = curr.left
+
+                node = stack[-1]  # peeking, don't pop yet
+
+                # if right child exists and not processed yet, go right
+                if node.right and lastVisited != node.right:
+                    curr = node.right
+                else:
+                    # both children done- visit node
+                    stack.pop()
+                    result.append(node.val)
+                    lastVisited = node
+
+            return result      
             
-            if node.left:
-                stack.append(node.left)   # push left
-            if node.right:
-                stack.append(node.right)  # push right (so right is processed first)
+        def recursive():
+            #Recursive solution
 
-        return result[::-1]      
+            result =[]
+
+            def dfs(node):
+                if node is None:
+                    return
+                dfs(node.left)
+                dfs(node.right)
+                result.append(node.val)
+            dfs(root)
+
+            return result
+
         
-        
-        #Recursive solution
-
-        result =[]
-
-        def dfs(node):
-            if node is None:
-                return
-            dfs(node.left)
-            dfs(node.right)
-            result.append(node.val)
-        dfs(root)
-
-        return result
+        #return iterativeLastVisited()
+        #return iterativeReverse()
+        return recursive()
