ed9f2d9a48a7832af70e535f25235a594b3e3414
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2026-02-17 11:07:08 -0500

    Time: 49 ms (83.05%) | Memory: 20.8 MB (39.03%) - LeetSync

--- tree/116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
+++ tree/116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.py
@@ -10,28 +10,52 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        if not root:
-            return root
-        
-        q = deque([root])
 
-        while q:
-            levelnode =[]
-            for i in range(len(q)):
-                node = q.popleft()
-                
-                levelnode.append(node)
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
+        curr , nxt = root, root.left if root else None
+
+        while curr and nxt:
+            curr.left.next = curr.right
+
+            if curr.next:
+                curr.right.next = curr.next.left
             
-            for i in range(len(levelnode)):
-                if i == len(levelnode)-1:
-                    levelnode[i].next = None
-                else:
-                    levelnode[i].next = levelnode[i+1]
+            curr = curr.next
+
+            if not curr:
+                curr = nxt
+                nxt = curr.left
+        return root        
+        
+        
+        
+        
+        
+        
+        
+        # I solved first uding below statemnt but it will take O(n) space to have to optimize and work for O(1) space 
+        # In below we are traversing to each levelnode and once we have data of all node at leevl we will assign the next one
+        # if not root:
+        #     return root
+        
+        # q = deque([root])
+
+        # while q:
+        #     levelnode =[]
+        #     for i in range(len(q)):
+        #         node = q.popleft()
+                
+        #         levelnode.append(node)
+        #         if node.left:
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)
+            
+        #     for i in range(len(levelnode)):
+        #         if i == len(levelnode)-1:
+        #             levelnode[i].next = None
+        #         else:
+        #             levelnode[i].next = levelnode[i+1]
    
 
-        return root
+        # return root
     
