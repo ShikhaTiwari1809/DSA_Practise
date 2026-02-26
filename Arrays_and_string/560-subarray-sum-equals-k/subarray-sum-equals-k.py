32945801473ffac7a09487d0624c505565445e3d
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2026-02-25 22:21:47 -0500

    Time: 47 ms (12.15%) | Memory: 23.5 MB (10.71%) - LeetSync

---
+++ Arrays_and_string/560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+from typing import List
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        freq = defaultdict(int)
+        freq[0] = 1  # empty prefix sum
+        prefix = 0
+        ans = 0
+
+        for x in nums:
+            prefix += x
+            ans += freq[prefix - k]
+            freq[prefix] += 1
+
+        return ans
