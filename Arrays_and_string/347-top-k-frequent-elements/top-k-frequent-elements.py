9dce430e6cef365f7e72c622a95dc25adb8a843a
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2026-03-19 14:26:32 -0400

    Time: 7 ms (51.08%) | Memory: 22.8 MB (90.45%) - LeetSync

--- Arrays_and_string/347-top-k-frequent-elements/top-k-frequent-elements.py
+++ Arrays_and_string/347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -2,9 +2,9 @@ from collections import Counter,defaultdict
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freq_count = Counter(nums)
-        print(freq_count)
+        
         sorted_freq = sorted(freq_count.items(), key=lambda item: item[1], reverse=True)
-        print(sorted_freq[:k])
+        
         ans = [i[0] for i in sorted_freq[:k] ]
         return ans
 
