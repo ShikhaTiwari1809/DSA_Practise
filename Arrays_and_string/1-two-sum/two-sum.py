b277c79488f6667e8d81d3f03ab114ca90171225
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2026-03-19 11:38:20 -0400

    Time: 1 ms (60.57%) | Memory: 20.4 MB (64.70%) - LeetSync

--- Arrays_and_string/1-two-sum/two-sum.py
+++ Arrays_and_string/1-two-sum/two-sum.py
@@ -4,8 +4,7 @@ class Solution:
 
         for i in range(len(nums)):
             if target - nums[i] in seen.keys():
-                print(seen)
                 return [i, seen[target - nums[i]]]
             seen[nums[i]] = i
-        print(seen)
+        
         return False
