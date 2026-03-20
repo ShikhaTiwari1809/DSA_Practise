676f6f06447e53dae700f05b7d6ab3c3f16f1096
Shikha Tiwari <104676075+ShikhaTiwari1809@users.noreply.github.com>
2026-03-20 11:52:22 -0400

    Time: 5 ms (38.86%) | Memory: 19.2 MB (89.99%) - LeetSync

---
+++ Arrays_and_string/36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,37 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        valid = ['1','2','3','4','5','6','7','8','9']
+        
+        # Check rows
+        for i in range(9):
+            row = []
+            for j in range(9):
+                if board[i][j] != ".":
+                    row.append(board[i][j])
+            if len(row) != len(set(row)):
+                return False
+        
+        # Check columns
+        for i in range(9):
+            col = []
+            for j in range(9):
+                if board[j][i] != ".":
+                    col.append(board[j][i])
+            if len(col) != len(set(col)):
+                return False
+
+        # Check 3x3 boxes
+        for box_row in range(0, 9, 3):
+            for box_col in range(0, 9, 3):
+                box = []
+                for i in range(box_row, box_row + 3):
+                    for j in range(box_col, box_col + 3):
+                        if board[i][j] != ".":
+                            box.append(board[i][j])
+                if len(box) != len(set(box)):
+                    return False   
+
+        
+        return True
+            
